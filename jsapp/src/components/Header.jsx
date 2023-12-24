96a1364667330c308ea4baebd781fef07083927e
Lyudmila <lugreat63@gmail.com>
2023-12-24 17:27:02 +0300

    12 lesson

---
+++ jsapp/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <img src='https://img.razrisyika.ru/kart/94/372802-logo-6.jpg'></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ jsapp/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img className='content__img' src='https://image.winudf.com/v2/image/Y29tLndhdGVyZmFsbHdhbGxwYXBlci5oZC53YXRlcmZhbGxwaWN0dXJlcy5waG90b3MuYmFja2dyb3VuZC5jdXRlLmNvb2wuYXJ0LndhdGVyZmFsbGltYWdlcy5oZC5mcmVlX3NjcmVlbl81XzE1MzMyNTAzMDZfMDc4/screen-5.jpg?fakeurl=1&type=.jpg'></img>
+            </div>
+            <div>ava + decription</div>
+            <div>
+                My posts
+                <div>new post</div>
+                <div>post 1</div>
+                <div>post 1</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
