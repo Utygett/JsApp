1ac3dd9debe68ba56d5432a59ca0e429c804bcee
Lyudmila <lugreat63@gmail.com>
2023-12-25 17:36:59 +0300

    lesson 14

--- jsapp/src/components/Header.jsx
+++ jsapp/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header>
+        <header className={s.Header}>
             <img src='https://img.razrisyika.ru/kart/94/372802-logo-6.jpg'></img>
         </header>
     )
--- jsapp/src/components/Profile.jsx
+++ jsapp/src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img className='content__img' src='https://image.winudf.com/v2/image/Y29tLndhdGVyZmFsbHdhbGxwYXBlci5oZC53YXRlcmZhbGxwaWN0dXJlcy5waG90b3MuYmFja2dyb3VuZC5jdXRlLmNvb2wuYXJ0LndhdGVyZmFsbGltYWdlcy5oZC5mcmVlX3NjcmVlbl81XzE1MzMyNTAzMDZfMDc4/screen-5.jpg?fakeurl=1&type=.jpg'></img>
             </div>
@@ -10,8 +11,8 @@ const Profile = () => {
             <div>
                 My posts
                 <div>new post</div>
-                <div>post 1</div>
-                <div>post 1</div>
+                <div className={s.item}>post 1</div>
+                <div className={s.item}>post 2</div>
             </div>
         </div>
     )
